b785093f6277a384f8537e18622765dc50172f0e
tarnishedmahin <mozahedul.hoque.mahin@gmail.com>
2026-03-28 04:03:40 +0600

    feat: added itinerary and activity database models

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -2,5 +2,7 @@ from app.models.user import User
 from app.models.community_entry import CommunityEntry
 from app.models.entry_photo import EntryPhoto
 from app.models.entry_video_embed import EntryVideoEmbed
+from app.models.itinerary import Itinerary, ItineraryActivity
 
-__all__ = ["User", "CommunityEntry", "EntryPhoto", "EntryVideoEmbed"]
+__all__ = ["User", "CommunityEntry", "EntryPhoto", "EntryVideoEmbed", "Itinerary", "ItineraryActivity"]
+
